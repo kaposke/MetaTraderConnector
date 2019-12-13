db4d2add86ada252b3e5685c6017d4db5663ff5a
kaposke <guilhermebassa@gmail.com>
2019-12-12 22:46:10 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetaTraderConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
